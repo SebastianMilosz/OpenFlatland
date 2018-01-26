54da432aee3144050f679a9405e80ead00366aff
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2018-01-26 23:13:35 +0100

    [ADD] GUIWidgetsLayer PART1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "world.h"
 #include "entityfactory.h"
+#include "guiwidgetslayer.h"
 #include "entity.h"
 
 #include <SFML/Graphics.hpp>
@@ -11,41 +12,32 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Test");
-    tgui::Gui gui(window); // Create the gui and attach it to the window
 
-    auto menu = tgui::MenuBar::create();
-    //menu->setRenderer(theme.getRenderer("MenuBar"));
-    menu->setSize((float)window.getSize().x, 22.f);
-    menu->addMenu("File");
-    menu->addMenuItem("Load");
-    menu->addMenuItem("Save");
-    menu->addMenuItem("Exit");
-    menu->addMenu("Edit");
-    menu->addMenuItem("Copy");
-    menu->addMenuItem("Paste");
-    menu->addMenu("Help");
-    menu->addMenuItem("About");
-    gui.add(menu);
-
+    GUIWidgetsLayer m_Widgets( window );
     World           m_World;
     EntityFactory   m_Factory( m_World );
 
     while (window.isOpen())
     {
+        bool guiConsumeEvent = false;
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            gui.handleEvent(event); // Pass the event to the widgets
+            guiConsumeEvent = m_Widgets.HandleEvent(event);
         }
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        if( guiConsumeEvent == false )
         {
-            int MouseX = sf::Mouse::getPosition(window).x;
-            int MouseY = sf::Mouse::getPosition(window).y;
-            std::shared_ptr<Entity> entity = m_Factory.Create( MouseX, MouseY, 0 );
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                int MouseX = sf::Mouse::getPosition(window).x;
+                int MouseY = sf::Mouse::getPosition(window).y;
+                std::shared_ptr<Entity> entity = m_Factory.Create( MouseX, MouseY, 0 );
+            }
         }
 
         /** Simulate the world */
@@ -55,7 +47,7 @@ int main()
 
         m_World.Draw( window );
 
-        gui.draw(); // Draw all widgets
+        m_Widgets.Draw();
 
         window.display();
     }
