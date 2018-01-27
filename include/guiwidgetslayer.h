bb413eb927305cf06a41781808419e04160c9b87
G580 <G580@localhost>
2018-01-27 18:07:17 +0100

    [ADD] Zoomin/Zoomout

--- include/guiwidgetslayer.h
+++ include/guiwidgetslayer.h
@@ -10,13 +10,19 @@ class GUIWidgetsLayer
         GUIWidgetsLayer( sf::RenderWindow& window );
         virtual ~GUIWidgetsLayer();
 
+        void AddGuiRegion( int x, int y, int w, int h );
+
+        bool MouseOnGui();
         bool HandleEvent( sf::Event& event );
         void Draw();
 
     protected:
 
     private:
-        tgui::Gui m_gui;
+        tgui::Gui           m_gui;
+        sf::RenderWindow&   m_window;
+
+        std::vector< sf::Rect<int> > m_guiRegions;
 };
 
 #endif // GUIWIDGETSLAYER_H
--- src/guiwidgetslayer.cpp
+++ src/guiwidgetslayer.cpp
@@ -1,7 +1,13 @@
 #include "guiwidgetslayer.h"
 
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
 GUIWidgetsLayer::GUIWidgetsLayer( sf::RenderWindow& window ) :
-    m_gui(window)
+    m_window( window ),
+    m_gui( window )
 {
     auto menu = tgui::MenuBar::create();
     //menu->setRenderer(theme.getRenderer("MenuBar"));
@@ -15,7 +21,7 @@ GUIWidgetsLayer::GUIWidgetsLayer( sf::RenderWindow& window ) :
     menu->addMenuItem("Paste");
     menu->addMenu("Help");
     menu->addMenuItem("About");
-    m_gui.add(menu);
+    m_gui.add( menu );
 
     auto button = tgui::Button::create();
     //button->setRenderer(theme.getRenderer("Button"));
@@ -24,19 +30,76 @@ GUIWidgetsLayer::GUIWidgetsLayer( sf::RenderWindow& window ) :
     button->setSize(100, 30);
     button->connect("pressed", [=](){  });
     m_gui.add(button);
+
+
+    auto child = tgui::ChildWindow::create();
+    //child->setRenderer(theme.getRenderer("ChildWindow"));
+    child->setSize(250, 120);
+    child->setPosition(420, 80);
+    child->setTitle("Child window");
+    m_gui.add(child);
 }
 
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
 GUIWidgetsLayer::~GUIWidgetsLayer()
 {
     //dtor
 }
 
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+bool GUIWidgetsLayer::MouseOnGui()
+{
+    int MouseX = sf::Mouse::getPosition(m_window).x;
+    int MouseY = sf::Mouse::getPosition(m_window).y;
+
+    tgui::Vector2f mousePos = tgui::Vector2f( MouseX, MouseY );
+
+    for (const auto& widget : m_gui.getWidgets())
+    {
+        if ( widget->mouseOnWidget( mousePos ) == true )
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
 bool GUIWidgetsLayer::HandleEvent( sf::Event& event )
 {
     return m_gui.handleEvent( event );
 }
 
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
 void GUIWidgetsLayer::Draw()
 {
     m_gui.draw();
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+void GUIWidgetsLayer::AddGuiRegion( int x, int y, int w, int h )
+{
+    sf::Rect<int> rec( x, y, w, h );
+    m_guiRegions.push_back( rec );
 }
