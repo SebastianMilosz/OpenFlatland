f9acde1fddcf891731c0c4f08ac2396bae8606a4
G580 <semilosz@interia.pl>
2018-08-21 10:13:21 +0200

    Update Version number

--- include/version.hpp
+++ include/version.hpp
@@ -2,7 +2,7 @@
 #define VERSION_H_INCLUDED
 
 const char*         APPLICATION_NAME            = "lifeSim";
-const unsigned int  APPLICATION_MINOR_VERSION   = 1;
+const unsigned int  APPLICATION_MINOR_VERSION   = 2;
 const unsigned int  APPLICATION_MINOR_MAJOR     = 0;
 
 #endif // VERSION_H_INCLUDED
