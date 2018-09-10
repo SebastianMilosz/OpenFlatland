051c042e35bf30c29ddb2f31dde73509c932cb4c
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2018-09-10 22:43:52 +0200

    [ADD] PerformanceLogger PART3

--- src/performancelogger.cpp
+++ src/performancelogger.cpp
@@ -2,13 +2,18 @@
 
 #include <utilities/MathUtilities.h>
 #include <utilities/LoggerUtilities.h>
+#include <utilities/SysUtilities.h>
+
+#include <iostream>
+#include <fstream>
 
 /*****************************************************************************/
 /**
   * @brief
  **
 ******************************************************************************/
-PerformanceLogger::PerformanceLogger()
+PerformanceLogger::PerformanceLogger() :
+    m_applicationId("")
 {
 
 }
@@ -42,6 +47,7 @@ PerformanceLogger& PerformanceLogger::GetInstance()
 ******************************************************************************/
 void PerformanceLogger::Initialize( std::string applicationId )
 {
+    m_applicationId = applicationId;
     LOGGER( LOG_INFO << "Performance Logger created for build: " << applicationId );
 }
 
@@ -52,7 +58,20 @@ void PerformanceLogger::Initialize( std::string applicationId )
 ******************************************************************************/
 void PerformanceLogger::SaveToFile( std::string filePath )
 {
+    std::ofstream performanceFile;
+    performanceFile.open ( filePath, std::fstream::in | std::fstream::out | std::fstream::app );
 
+    performanceFile << m_applicationId << ",";
+
+    std::map<unsigned int , PerformanceData>::iterator it;
+    for ( it = m_PerformanceMap.begin(); it != m_PerformanceMap.end(); it++ )
+    {
+        performanceFile << ',' << it->second.Name << ',' << it->second.DurationTime;
+    }
+
+     performanceFile << '\n';
+
+    performanceFile.close();
 }
 
 /*****************************************************************************/
@@ -62,7 +81,7 @@ void PerformanceLogger::SaveToFile( std::string filePath )
 ******************************************************************************/
 void PerformanceLogger::AddPerformancePoint( unsigned int id, std::string name )
 {
-
+    m_PerformanceMap[ id ].Name = name;
 }
 
 /*****************************************************************************/
@@ -72,7 +91,7 @@ void PerformanceLogger::AddPerformancePoint( unsigned int id, std::string name )
 ******************************************************************************/
 void PerformanceLogger::PerformancePointEnter( unsigned int id )
 {
-
+    m_PerformanceMap[ id ].StartTime = utilities::system::GetTime();
 }
 
 /*****************************************************************************/
@@ -82,5 +101,7 @@ void PerformanceLogger::PerformancePointEnter( unsigned int id )
 ******************************************************************************/
 void PerformanceLogger::PerformancePointLeave( unsigned int id )
 {
-
+    double prewDuration = m_PerformanceMap[ id ].DurationTime;
+    double newDuration  = (utilities::system::GetTime() - m_PerformanceMap[ id ].StartTime);
+    m_PerformanceMap[ id ].DurationTime = (prewDuration+newDuration)/2.0F;
 }
