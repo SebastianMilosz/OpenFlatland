d982821be14e38eacfba8dfffe8881acdf4f15b0
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2018-09-26 23:25:50 +0200

    [ADD] Property link PART5

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 111
-#define MERCURIAL_DATE_TIME "Tue Sep 25 22:22:56 2018 +0200"
+#define MERCURIAL_REVISION 112
+#define MERCURIAL_DATE_TIME "Wed Sep 26 22:23:45 2018 +0200"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
--- libraries/codeframe-master/src/serializablepropertyinfo.cpp
+++ libraries/codeframe-master/src/serializablepropertyinfo.cpp
@@ -2,5 +2,211 @@
 
 namespace codeframe
 {
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+void cPropertyInfo::Init()
+{
+    m_description   = "";
+    m_kind          = KIND_NON;
+    m_xmlmode       = XMLMODE_RW;
+    m_eventEnable   = true;
+    m_min           = INT_MIN;
+    m_max           = INT_MAX;
+    m_enable        = true;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo::cPropertyInfo() :
+    m_refmgr(),
+    m_serializableParent(NULL)
+{
+    Init();
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo::cPropertyInfo(const cPropertyInfo& sval) :
+    m_description(sval.m_description),
+    m_kind(sval.m_kind),
+    m_enumArray(sval.m_enumArray),
+    m_eventEnable(sval.m_eventEnable),
+    m_min(sval.m_min),
+    m_max(sval.m_max),
+    m_enable(sval.m_enable) ,
+    m_register(sval.m_register),
+    m_xmlmode(sval.m_xmlmode),
+    m_refmgr(sval.m_refmgr),
+    m_serializableParent(sval.m_serializableParent)
+{
+
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo::cPropertyInfo(const cPropertyInfo& sval, cSerializableInterface* serializableParent ) :
+    m_description(sval.m_description),
+    m_kind(sval.m_kind),
+    m_enumArray(sval.m_enumArray),
+    m_eventEnable(sval.m_eventEnable),
+    m_min(sval.m_min),
+    m_max(sval.m_max),
+    m_enable(sval.m_enable) ,
+    m_register(sval.m_register),
+    m_xmlmode(sval.m_xmlmode),
+    m_refmgr(sval.m_refmgr),
+    m_serializableParent( serializableParent )
+{
+    m_refmgr.SetParent( m_serializableParent );
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Register( eREG_MODE mod,
+                                         uint16_t  reg,
+                                         uint16_t  regSize,
+                                         uint16_t  cellOffset,
+                                         uint16_t  cellSize,
+                                         uint16_t  bitMask
+                                       )
+{
+    m_register.Set( mod, reg, regSize, cellOffset, cellSize, bitMask);
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Description( const std::string& desc )
+{
+    m_description = desc;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Kind( eKind kind )
+{
+    m_kind = kind;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Enum( const std::string& enuma )
+{
+    m_enumArray = enuma;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::ReferencePath( const std::string& referencePath )
+{
+    m_refmgr.SetReference( referencePath, m_serializableParent );
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Event( int e )
+{
+    m_eventEnable = e;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Min( int min )
+{
+    m_min = min;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Max( int max )
+{
+    m_max = max;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::Enable( int state )
+{
+    m_enable = state;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::XMLMode( eXMLMode mode )
+{
+    m_xmlmode = mode;
+    return *this;
+}
+
+/*****************************************************************************/
+/**
+  * @brief
+ **
+******************************************************************************/
+cPropertyInfo& cPropertyInfo::operator=(cPropertyInfo val)
+{
+    m_description        = val.m_description;
+    m_kind               = val.m_kind;
+    m_xmlmode            = val.m_xmlmode;
+    m_enumArray	         = val.m_enumArray;
+    m_register           = val.m_register;
+    m_eventEnable        = val.m_eventEnable;
+    m_min                = val.m_min;
+    m_max                = val.m_max;
+    m_enable             = val.m_enable;
+    m_refmgr             = val.m_refmgr;
+    m_serializableParent = val.m_serializableParent;
+    return *this;
+}
 
 }
