490546115da1979cd7df72e0d9a78df1090c7036
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2018-10-26 22:57:29 +0200

    [ADD] ReferenceManager PART1

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 129
-#define MERCURIAL_DATE_TIME "Thu Oct 25 22:33:42 2018 +0200"
+#define MERCURIAL_REVISION 130
+#define MERCURIAL_DATE_TIME "Fri Oct 26 21:10:02 2018 +0200"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
