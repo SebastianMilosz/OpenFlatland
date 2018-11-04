7d6b4dd4b79781851842da539d923ef214cafe08
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2018-11-04 13:26:50 +0100

    [TMP] TMP5

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 136
-#define MERCURIAL_DATE_TIME "Sat Nov 03 23:03:14 2018 +0100"
+#define MERCURIAL_REVISION 137
+#define MERCURIAL_DATE_TIME "Sat Nov 03 23:22:09 2018 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
