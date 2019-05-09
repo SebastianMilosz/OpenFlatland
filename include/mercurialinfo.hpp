3556c1097ca37aa745be6779f8a0f9f2027fa3ce
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2019-05-09 14:45:15 +0200

    [ADD] OnWeightDimensionsVectorChanged Implementation

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 158
-#define MERCURIAL_DATE_TIME "Thu Apr 11 13:07:11 2019 +0200"
+#define MERCURIAL_REVISION 159
+#define MERCURIAL_DATE_TIME "Fri Apr 19 13:00:35 2019 +0200"
 #define MERCURIAL_AUTHOR "G580 <semilosz@interia.pl>"
 
 #endif
