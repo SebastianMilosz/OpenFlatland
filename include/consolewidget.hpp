d5816ddff3d3c37efd41bc89fea9f648c250a08d
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2019-08-01 23:26:36 +0200

    [ADD] Lua scripting functionality part 5

--- include/consolewidget.hpp
+++ include/consolewidget.hpp
@@ -6,6 +6,7 @@
 #include <serializable.hpp>
 #include <utilities/LoggerUtilities.h>
 #include <utilities/FileUtilities.h>
+#include <utilities/DataTypesUtilities.h>
 
 using namespace codeframe;
 
@@ -21,24 +22,23 @@ class ConsoleWidget : public sigslot::has_slots<>
 
     private:
         // Portable helpers
-        static int   Stricmp(const char* str1, const char* str2)         { int d; while ((d = toupper(*str2) - toupper(*str1)) == 0 && *str1) { str1++; str2++; } return d; }
-        static int   Strnicmp(const char* str1, const char* str2, int n) { int d = 0; while (n > 0 && (d = toupper(*str2) - toupper(*str1)) == 0 && *str1) { str1++; str2++; n--; } return d; }
-        static char* Strdup(const char *str)                             { size_t len = strlen(str) + 1; void* buf = malloc(len); IM_ASSERT(buf); return (char*)memcpy(buf, (const void*)str, len); }
-        static void  Strtrim(char* str)                                  { char* str_end = str + strlen(str); while (str_end > str && str_end[-1] == ' ') str_end--; *str_end = 0; }
+        //static int   Stricmp(const char* str1, const char* str2)         { int d; while ((d = toupper(*str2) - toupper(*str1)) == 0 && *str1) { str1++; str2++; } return d; }
+        //static int   Strnicmp(const char* str1, const char* str2, int n) { int d = 0; while (n > 0 && (d = toupper(*str2) - toupper(*str1)) == 0 && *str1) { str1++; str2++; n--; } return d; }
+        //static char* Strdup(const char *str)                             { size_t len = strlen(str) + 1; void* buf = malloc(len); IM_ASSERT(buf); return (char*)memcpy(buf, (const void*)str, len); }
+        //static void  Strtrim(char* str)                                  { char* str_end = str + strlen(str); while (str_end > str && str_end[-1] == ' ') str_end--; *str_end = 0; }
 
         void AddLog(const char* fmt, ...) IM_FMTARGS(2);
         static int TextEditCallbackStub( ImGuiInputTextCallbackData* data );
         int TextEditCallback( ImGuiInputTextCallbackData* data );
 
-        cSerializableInterface& m_parent;
-        ImGuiTextBuffer         m_Buf;
-        ImGuiTextFilter         m_Filter;
-        ImVector<int>           m_LineOffsets;        // Index to lines offset
-        bool                    m_ScrollToBottom;
-        char                    m_InputBuf[256];
-        ImVector<const char*>   m_Commands;
-        ImVector<char*>         m_History;
-        int                     m_HistoryPos;    // -1: new line, 0..History.Size-1 browsing history.
+        cSerializableInterface&                         m_parent;
+        ImGuiTextBuffer                                 m_Buf;
+        ImGuiTextFilter                                 m_Filter;
+        ImVector<int>                                   m_LineOffsets;
+        bool                                            m_ScrollToBottom;
+        char                                            m_InputBuf[256];
+        ImVector<const char*>                           m_Commands;
+        utilities::data::CircularBuffer<32, std::string> m_History;
 };
 
 #endif // LOGWIDGET_HPP
