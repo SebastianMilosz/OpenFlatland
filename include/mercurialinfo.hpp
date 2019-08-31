87857acdac5b38fcf13514c98d5862e579a434a4
G580 <G580@localhost>
2019-08-31 10:32:44 +0200

    [ADD] AplicationDataStorage PART4

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 171
-#define MERCURIAL_DATE_TIME "Wed Aug 28 22:14:29 2019 +0200"
+#define MERCURIAL_REVISION 172
+#define MERCURIAL_DATE_TIME "Sat Aug 31 10:15:29 2019 +0200"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
