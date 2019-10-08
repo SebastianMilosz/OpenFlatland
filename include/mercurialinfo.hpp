fcdd3e38adfa58ae4655f6b0b665b76a9e9a0a63
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2019-10-08 22:58:22 +0200

    CMake PART4

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 177
-#define MERCURIAL_DATE_TIME "Sun Sep 29 23:23:46 2019 +0200"
+#define MERCURIAL_REVISION 180
+#define MERCURIAL_DATE_TIME "Sun Oct 06 21:03:57 2019 +0200"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
