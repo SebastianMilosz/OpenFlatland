cba6d4cc698bf322acd62f1ab1f49c6cb112ce7c
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2019-10-21 22:37:21 +0200

    Test #1

--- libraries/codeframe-master/tests/test_serializablelibmain.cpp
+++ libraries/codeframe-master/tests/test_serializablelibmain.cpp
@@ -1,9 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
+#include <iostream>
+#include <conio.h>
+
 int main(int argc, char *argv[])
-{      
+{
     int result = Catch::Session().run( argc, argv );
+
+    do
+    {
+        std::cout << '\n' << "Press a key to continue...";
+    } while ( std::cin.get() != '\n' );
 
     return ( result < 0xff ? result : 0xff );
 }
