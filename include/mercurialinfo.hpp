70886aec1d6b1b0d80cbba612cd050d26f11d44b
G580 <G580@localhost>
2019-10-22 22:00:52 +0200

    Test #2

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 194
-#define MERCURIAL_DATE_TIME "Sat Oct 19 23:21:14 2019 +0200"
+#define MERCURIAL_REVISION 195
+#define MERCURIAL_DATE_TIME "Mon Oct 21 22:37:21 2019 +0200"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
