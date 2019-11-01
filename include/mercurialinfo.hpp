0d0b9f4db3747720d3f739fdc21e258014a4fe2d
G580 <G580@localhost>
2019-11-01 22:03:06 +0100

    Test #5

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 200
-#define MERCURIAL_DATE_TIME "Wed Oct 30 23:01:58 2019 +0100"
+#define MERCURIAL_REVISION 201
+#define MERCURIAL_DATE_TIME "Thu Oct 31 22:23:38 2019 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
--- libraries/codeframe-master/tests/test_serializable_path.cpp
+++ libraries/codeframe-master/tests/test_serializable_path.cpp
@@ -1,26 +1,88 @@
 #include "catch.hpp"
 
 #include <serializable.hpp>
+#include <serializablecontainer.hpp>
+
+using namespace codeframe;
 
 TEST_CASE( "Serializable library path", "[serializable.Path]" )
 {
-    class classTestSerializable : public codeframe::cSerializable
+    class classTest_Static : public codeframe::cSerializable
     {
         public:
-            CODEFRAME_META_CLASS_NAME( "classTestSerializable" );
+            CODEFRAME_META_CLASS_NAME( "classTest_Static" );
             CODEFRAME_META_BUILD_TYPE( codeframe::STATIC );
 
         public:
-            classTestSerializable( const std::string& name, cSerializableInterface* parent ) : cSerializable( name, parent )
+            classTest_Static( const std::string& name, cSerializableInterface* parent ) : cSerializable( name, parent )
             {
-
             }
     };
 
-    classTestSerializable testSerializable( "testName", NULL );
-
-    SECTION( "resizing bigger changes size and capacity" )
+    class classTest_Dynamic : public codeframe::cSerializable
     {
-        REQUIRE( testSerializable.Identity().ObjectName() == "testName" );
+        public:
+            CODEFRAME_META_CLASS_NAME( "classTest_Dynamic" );
+            CODEFRAME_META_BUILD_TYPE( codeframe::DYNAMIC );
+
+        codeframe::Property<int> Property1;
+        codeframe::Property<int> Property2;
+        codeframe::Property<int> Property3;
+        codeframe::Property<int> Property4;
+
+        public:
+            classTest_Dynamic( const std::string& name, cSerializableInterface* parent ) :
+                cSerializable( name, parent ),
+                Property1( this, "Property1", 100U , cPropertyInfo().Kind( KIND_NUMBER ).Description("Property1_desc") ),
+                Property2( this, "Property2", 200U , cPropertyInfo().Kind( KIND_NUMBER ).Description("Property2_desc") ),
+                Property3( this, "Property3", 300U , cPropertyInfo().Kind( KIND_NUMBER ).Description("Property3_desc") ),
+                Property4( this, "Property4", 400U , cPropertyInfo().Kind( KIND_NUMBER ).Description("Property4_desc") )
+            {
+            }
+    };
+
+    class classTest_Container : public codeframe::cSerializableContainer
+    {
+        public:
+            CODEFRAME_META_CLASS_NAME( "classTest_Container" );
+            CODEFRAME_META_BUILD_TYPE( codeframe::STATIC );
+
+            virtual smart_ptr<cSerializableInterface> Create(
+                                                             const std::string& className,
+                                                             const std::string& objName,
+                                                             const std::vector<codeframe::VariantValue>& params = std::vector<codeframe::VariantValue>()
+                                                             )
+            {
+                if ( className == "classTest_Dynamic" )
+                {
+                    smart_ptr<classTest_Dynamic> obj = smart_ptr<classTest_Dynamic>( new classTest_Dynamic( objName, this ) );
+
+                    int id = InsertObject( obj );
+
+                    return obj;
+                }
+
+                return smart_ptr<codeframe::cSerializableInterface>();
+            }
+
+        public:
+            classTest_Container( const std::string& name, cSerializableInterface* parent ) : cSerializableContainer( name, parent )
+            {
+            }
+    };
+
+    classTest_Static staticSerializableObject( "testNameStatic", NULL );
+
+    classTest_Container staticContainerObject( "testNameContainerStatic", &staticSerializableObject );
+
+    staticContainerObject.Create( "classTest_Dynamic", "node" );
+    staticContainerObject.Create( "classTest_Dynamic", "node" );
+    staticContainerObject.Create( "classTest_Dynamic", "node" );
+    staticContainerObject.Create( "classTest_Dynamic", "node" );
+    staticContainerObject.Create( "classTest_Dynamic", "node" );
+
+    SECTION( "Basic serializable objects tests" )
+    {
+        REQUIRE( staticSerializableObject.Identity().ObjectName() == "testNameStatic" );
     }
 }
