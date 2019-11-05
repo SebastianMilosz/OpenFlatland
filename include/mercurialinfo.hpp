22165c00191079e47b0782e768acbc0e541c7858
G580 <G580@localhost>
2019-11-05 22:45:37 +0100

    Fix UID in serializable identity

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 206
-#define MERCURIAL_DATE_TIME "Sun Nov 03 22:19:30 2019 +0100"
+#define MERCURIAL_REVISION 207
+#define MERCURIAL_DATE_TIME "Mon Nov 04 23:04:29 2019 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
