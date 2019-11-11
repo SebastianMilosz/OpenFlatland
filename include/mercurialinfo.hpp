64075c299f70ec754c35434ea55017c8535fc9ba
G580 <G580@localhost>
2019-11-11 21:52:18 +0100

    PropertySelection #3

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 211
-#define MERCURIAL_DATE_TIME "Fri Nov 08 22:57:44 2019 +0100"
+#define MERCURIAL_REVISION 212
+#define MERCURIAL_DATE_TIME "Sun Nov 10 22:29:49 2019 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
