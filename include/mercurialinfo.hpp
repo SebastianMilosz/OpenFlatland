63fe8c42951adea42743529f79b2b3d7638438c5
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2019-11-24 20:58:05 +0100

    Box2D-MT #1

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 231
-#define MERCURIAL_DATE_TIME "Sun Nov 24 19:04:15 2019 +0100"
+#define MERCURIAL_REVISION 232
+#define MERCURIAL_DATE_TIME "Sun Nov 24 19:06:51 2019 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
