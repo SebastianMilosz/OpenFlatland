08c9b63ea5a1c63bae837837f55077f9e2cd911f
G580 <G580@localhost>
2019-12-03 21:42:52 +0100

    Rays Angle #7

--- include/mercurialinfo.hpp
+++ include/mercurialinfo.hpp
@@ -1,8 +1,8 @@
 #ifndef MercurialRevisionH
 #define MercurialRevisionH
 
-#define MERCURIAL_REVISION 238
-#define MERCURIAL_DATE_TIME "Thu Nov 28 22:46:49 2019 +0100"
+#define MERCURIAL_REVISION 239
+#define MERCURIAL_DATE_TIME "Sun Dec 01 21:12:19 2019 +0100"
 #define MERCURIAL_AUTHOR "G580"
 
 #endif
