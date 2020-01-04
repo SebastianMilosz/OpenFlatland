1ff0bec3777298697feeebb8d7755721b161fdb3
G580 <38083728+SebastianMilosz@users.noreply.github.com>
2020-01-04 13:43:25 +0100

    VisionViewerWidget #2

--- src/entity_vision_viewer_widget.cpp
+++ src/entity_vision_viewer_widget.cpp
@@ -7,7 +7,7 @@
 ******************************************************************************/
 VisionViewerWidget::VisionViewerWidget()
 {
-    //ctor
+    m_displayTexture.create(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 /*****************************************************************************/
@@ -28,10 +28,6 @@ VisionViewerWidget::~VisionViewerWidget()
 void VisionViewerWidget::SetObject( smart_ptr<codeframe::ObjectNode> obj )
 {
     m_objEntity = smart_dynamic_pointer_cast<Entity>(obj);
-
-    m_displayImage.create(640, 480, sf::Color::Black);
-    m_displayTexture.create(640, 480);
-    m_displayTexture.update(m_displayImage);
 }
 
 /*****************************************************************************/
@@ -50,17 +46,38 @@ void VisionViewerWidget::Draw( const char* title, bool* p_open )
 
     ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2(2,2) );
 
-    ImVec2 vec;
-    vec.x = (ImGui::GetWindowSize().x - 640) * 0.5f;
-    vec.y = (ImGui::GetWindowSize().y - 480) * 0.5f;
+    if ( smart_ptr_isValid(m_objEntity) )
+    {
+        // Center vision screen inside imgui widget
+        m_cursorPos = ImGui::GetWindowSize();
+        m_cursorPos.x = (m_cursorPos.x - SCREEN_WIDTH) * 0.5f;
+        m_cursorPos.y = (m_cursorPos.y - SCREEN_HEIGHT) * 0.5f;
+        ImGui::SetCursorPos( m_cursorPos );
 
-    ImGui::SetCursorPos( vec );
+        EntityVision& vision = m_objEntity->Vision();
 
+        const std::vector<float>& distanceVector = vision.GetDistanceVector();
 
+        const unsigned int w = std::ceil((const float)SCREEN_WIDTH / (const float)distanceVector.size());
 
-    m_displayTexture.update(m_displayImage);
+        m_displayTexture.clear();
 
-    ImGui::Image( m_displayTexture, sf::Color::White, sf::Color::White );
+        unsigned int x_rec = 0;
+
+        for ( const auto distance : distanceVector )
+        {
+            float h = SCREEN_HEIGHT - distance/SCREEN_HEIGHT * DISTANCE_TO_SCREEN_FACTOR;
+            float y_rec = (SCREEN_HEIGHT / 2.0F) - (h / 2.0F);
+
+            m_rectangle.setPosition(x_rec, y_rec);
+            m_rectangle.setSize( sf::Vector2f(w, h) );
+            m_displayTexture.draw(m_rectangle);
+            x_rec += w;
+        }
+
+        m_displayTexture.display();
+        ImGui::Image( m_displayTexture.getTexture() );
+    }
 
     ImGui::PopStyleVar();
     ImGui::End();
