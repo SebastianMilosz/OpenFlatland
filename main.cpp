907744d1422cae7e0b3fb77f47e1bb79535c54cd
SebastianMilosz <semilosz@interia.pl>
2025-02-02 17:57:14 +0100

    migration part11

--- main.cpp
+++ main.cpp
@@ -13,7 +13,14 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "" );
+
     Application      application("Application", window);
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
     while (window.isOpen())
     {
